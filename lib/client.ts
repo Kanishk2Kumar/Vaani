7c629500fde21727c411b4dafe6df9fd9918d8a9
sarvadandge29 <sarvadandge@gmail.com>
2025-06-09 23:44:26 +0530

    Supabase setup done

--- lib/client.ts
+++ lib/client.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://xcnigjohlmsohjovsdsl.supabase.co'
-const supabaseKey = ""
+const supabaseKey = "Enter your key"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
